60944f4c9b7050ad4fdf673088c201ed5e0684b6
Fabien Le Frapper <contact@fabienlefrapper.me>
2018-05-25 18:33:10 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
-const Koa = require('koa')
+import Koa from 'koa'
+import koaRouter from 'koa-router'
+import koaBody from 'koa-bodyparser'
+import { graphqlKoa } from 'apollo-server-koa'
 
 const app = new Koa()
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
 
-// response
-console.log(`API is listening on port ${PORT} in ${NODE_ENV} mode`);
+// koaBody is needed just for POST.
+router.post('/graphql', koaBody(), graphqlKoa({ schema: myGraphQLSchema }));
+router.get('/graphql', graphqlKoa({ schema: myGraphQLSchema }));
 
-app.listen(PORT)
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// response
+console.log(`API is listening on port ${PORT} in ${NODE_ENV} mode`)
+
+app.listen(PORT);
+
